75a8a360f1616978dc0a8535f969bbb231bfce90
Mathias Toala <fetoala@outlook.com>
2025-08-04 18:41:48 -0500

    first shot at a slash command

--- commands/utility/comando.ts
+++ commands/utility/comando.ts
@@ -1,7 +1,18 @@
 // const { SlashCommandBuilder } = require("discord.js");
+import { SlashCommandBuilder } from "discord.js";
 
 // new SlashCommandBuilder().setName('Perritos').setDescription('Trae perritos de mi API favorita');
 
 // async execute(interaction) {
 // 	await interaction.reply('Pong!')
 // }
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('Perritos').setDescription('Trae perritos de mi API favorita'),
+    async execute(interaction: any){
+        let perritoIMG = await fetch('https://dog.ceo/api/breeds/image/random');
+        let promesa = await perritoIMG.json();
+        let imagen = promesa.message;
+        await interaction.reply(imagen);
+    }
+}
