dba5cf3b4ea6bac87f5be1c8cf4525278200f584
Mathias Toala <fetoala@outlook.com>
2025-08-04 18:32:37 -0500

    Commands! working on it

---
+++ commands/utility/comando.ts
@@ -0,0 +1,7 @@
+// const { SlashCommandBuilder } = require("discord.js");
+
+// new SlashCommandBuilder().setName('Perritos').setDescription('Trae perritos de mi API favorita');
+
+// async execute(interaction) {
+// 	await interaction.reply('Pong!')
+// }
